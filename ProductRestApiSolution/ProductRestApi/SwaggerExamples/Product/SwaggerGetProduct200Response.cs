44a205d4220cd4a22e8dac4d67484520256341ae
Eren ÖZTEN <erenozten@gmail.com>
2025-05-03 02:41:56 +0300

    remove unnecessary usings

--- ProductRestApiSolution/ProductRestApi/SwaggerExamples/Product/SwaggerGetProduct200Response.cs
+++ ProductRestApiSolution/ProductRestApi/SwaggerExamples/Product/SwaggerGetProduct200Response.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Swashbuckle.AspNetCore.Filters;
 using ProductRestApi.Common.Responses;
-using ProductRestApi.DTOs;
 using ProductRestApi.DTOs.Product;
 
 namespace ProductRestApi.SwaggerExamples.Product
