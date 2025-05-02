ea25ea3c362ae078c67c3471d2c8a8664701ca54
Eren ÖZTEN <erenozten@gmail.com>
2025-05-03 02:19:30 +0300

    get rid of magic strings on LoggingTemplates.cs

--- ProductRestApiSolution/ProductRestApi/Common/Logging/LoggingTemplates.cs
+++ ProductRestApiSolution/ProductRestApi/Common/Logging/LoggingTemplates.cs
@@ -2,15 +2,15 @@ namespace ProductRestApi.Common.Logging;
 
 public static class LoggingTemplates
 {
-    public const string ProductNotFoundError = "{@ProductLogModel} Ürün bulunamadı.";
-    public const string ProductDeletedSuccessfully = "{@ProductLogModel} Product deleted successfully.";
-    public const string InvalidIdError = "{@ProductLogModel} Id is invalid.";
-    public const string ValidationError = "{@ProductLogModel} Validation Error.";
-    public const string ProductNameDuplicateError = "{@ProductLogModel} Product Name is not valid.";
-    public const string ProductUpdated = "{@ProductLogModel} Product updated successfully.";
-    public const string ProductFoundButDeletionError = "{@ProductLogModel} Product found but an error occurred during deletion.";
-    public const string ProductsFound = "{@ProductLogModel} Products found.";
-    
+    public const string ProductNotFoundError = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Ürün bulunamadı.";
+    public const string ProductDeletedSuccessfully = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Product deleted successfully.";
+    public const string InvalidIdError = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Id is invalid.";
+    public const string ValidationError = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Validation Error.";
+    public const string ProductNameDuplicateError = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Product Name is not valid.";
+    public const string ProductUpdated = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Product updated successfully.";
+    public const string ProductFoundButDeletionError = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Product found but an error occurred during deletion.";
+    public const string ProductsFound = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Products found.";
+    public const string ProductCreatedSuccessfully = $"{{@{LoggingMessageTemplate.ProductLogModel}}} Ürün başarıyla oluşturuldu.";
     
     
 }
