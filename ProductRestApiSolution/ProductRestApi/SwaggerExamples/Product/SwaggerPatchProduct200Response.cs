454a27bbb24d5de27c7bc4506c6452976c78d646
Eren ÖZTEN <erenozten@gmail.com>
2025-05-03 02:40:33 +0300

    create new Swagger response examples for PATCH and PUT

---
+++ ProductRestApiSolution/ProductRestApi/SwaggerExamples/Product/SwaggerPatchProduct200Response.cs
@@ -0,0 +1,22 @@
+using Swashbuckle.AspNetCore.Filters;
+using ProductRestApi.Common.Responses;
+using ProductRestApi.DTOs.Product;
+
+namespace ProductRestApi.SwaggerExamples.Product
+{
+    public class SwaggerPatchProduct200Response : IExamplesProvider<GenericApiResponse<ProductGetResponseDto>>
+    {
+        public GenericApiResponse<ProductGetResponseDto> GetExamples()
+        {
+            return GenericApiResponse<ProductGetResponseDto>.Success(
+                statusCode: StatusCodes.Status200OK,
+                data: new ProductGetResponseDto
+                {
+                    Id = 13,
+                    Name = "Paris",
+                    About = "A wonderful product"
+                },
+                message: "");
+        }
+    }
+}
